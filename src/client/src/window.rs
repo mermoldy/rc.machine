7b81627851679c9aca7fd61971e48a09fd1e5819
Serhii Babak <s.babak@scalr.com>
2020-05-25 23:31:27 +0300

    fix assets

--- src/client/src/window.rs
+++ src/client/src/window.rs
@@ -51,7 +51,8 @@ impl Window {
                 .exit_on_esc(true)
                 .build()?;
 
-        let ttf_assets = find_folder::Search::ParentsThenKids(1, 4).for_folder("assets/ttf")?;
+        let ttf_assets =
+            find_folder::Search::ParentsThenKids(1, 4).for_folder("assets/ttf/firacode")?;
         let glyphs = window.load_font(ttf_assets.join("FiraCode-Medium.ttf"))?;
 
         let canvas = image::ImageBuffer::new(width as u32, height as u32);
